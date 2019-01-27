0c1a5194cc7cee48353ad9ad1bcdd1e238b99d03
Harry <harry@Harrys-MacBook-Pro-701.local>
2019-01-27 13:38:34 -0600

    Finished basic functionality

--- ws2812/version_2/version_2.ino
+++ ws2812/version_2/version_2.ino
@@ -1,6 +1,16 @@
 #define FASTLED_ESP8266_D1_PIN_ORDER
 
 #include <FastLED.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+//WiFi configuration
+const char* ssid = "Lisby 2G";
+const char* password = "lisby12345";
+//In the future add AP function for unknown networks
+
 
 #define LED_PIN     5
 #define NUM_LEDS    20
@@ -45,11 +55,76 @@ void setup() {
     
     currentPalette = RainbowColors_p;
     currentBlending = LINEARBLEND;
+
+    Serial.println("Booting");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+    }
+    // Port defaults to 8266
+    // ArduinoOTA.setPort(8266);
+
+    // Hostname defaults to esp8266-[ChipID]
+    ArduinoOTA.setHostname("DesktopRGB");
+
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else { // U_SPIFFS
+            type = "filesystem";
+        }
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+    });
+
+
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+
+
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+          Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+          Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+          Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+          Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+          Serial.println("End Failed");
+        }
+    });
+
+
+    ArduinoOTA.begin();
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 bool firstTime = true;
 bool overrideBrightness=true;
 int setBrightness = 127;
+uint32_t lastTime = 0;
 
 void loop()
 {
@@ -57,13 +132,11 @@ void loop()
     currentPalette = PartyColors_p;
     
     static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
+    startIndex = startIndex + 1; /* motion speed */ 
 
-    static uint32_t lastTime = 0; 
-
-    if((lastTime-millis())>500){
+    if((millis()-lastTime)>1000){
         lastTime=millis();
-        Serial.println(startIndex);
+        //Serial.println(startIndex);
     }   
     FillLEDsFromPaletteColors(startIndex);
     
